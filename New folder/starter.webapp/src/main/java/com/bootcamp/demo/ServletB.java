73127e463deb0d5b47fd0b1c0291c53f67edc04f
C0rvs <cyrus080600@gmail.com>
2022-11-04 14:29:33 +0800

    Initial commit

---
+++ New folder/starter.webapp/src/main/java/com/bootcamp/demo/ServletB.java
@@ -0,0 +1,24 @@
+package com.bootcamp.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ServletB extends HttpServlet {
+
+    private static final long serialVersionUID = 2265793564953959394L;
+    
+    private static final Logger LOGGER = LogManager.getLogger(ServletB.class);
+
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        LOGGER.info("action=do_get, source=servlet_b");
+    }
+
+}
